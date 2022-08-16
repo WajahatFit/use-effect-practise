22cd4e8c2f493ccddb20b18266b300eea2f0b779
Syed Wajahat Ali <wajahat.klk25.com>
2022-08-16 20:56:53 +0200

    it3 issues

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -6,16 +6,45 @@ export default function Counter() {
   const [dynamicClass, setDynamicClass] = useState('text');
 
   // First iteration
-
+    const handleIncrease = () => {
+      setCount(prev => prev + 1)
+    }
   // Second and Fourth iteration
+    //Colors of numbers
+    switch(count) {
+      case count <= 0:
+        setDynamicClass('red')
+        break;
+      case count > 0 && count <= 5:
+       setDynamicClass('orange') 
+        break;
+      case count > 5:
+        setDynamicClass('green');
+        break;
+    default:
+          setDynamicClass('text')
+    }
 
+     //random num between 0 & 10
+  const randomNum = Math.floor(Math.random() * 10)
+
+  
+  useEffect(()=> {
+    console.log('component mounting') 
+    setCount(randomNum)
+  }, [])
+
+
+    const handleDecrese = () => {
+      setCount(prev => prev - 1)
+    }
   // Third iteration
 
   return (
     <div>
       <p>Counter: <span className={dynamicClass}>{count}</span></p>
-      <button>+ Increase</button>
-      <button>- Decrease</button>
+      <button onClick={handleIncrease}>+ Increase</button>
+      <button onClick={handleDecrese}>- Decrease</button>
     </div>
   )
 }
